d58b1721f1a555750ce9fa7ebae81a972717790b
Deniz van Iersel <deniz.van.iersel@fesma.nl>
2022-12-02 13:54:38 +0100

    Add looping in Java example from course. The looping class runs successfully and prints that "Hello World!" 13 times.

---
+++ src/main/java/nl/fesma/absolute/beginners/course/Looping.java
@@ -0,0 +1,10 @@
+package nl.fesma.absolute.beginners.course;
+
+
+public class Looping {
+    public static void main(String[] args) {
+        for (int i=1; i<=13; i++){
+            System.out.println("Hello World!");
+        }
+    }
+}
